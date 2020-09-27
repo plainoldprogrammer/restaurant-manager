2e3a0b7975f6c6be72ef40cf178cb85ebe9479a3
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-09-27 17:30:41 -0500

    renaming the NavItems for Menu and for Salaries

--- WebApplication/ClientApp/src/components/NavMenu.js
+++ WebApplication/ClientApp/src/components/NavMenu.js
@@ -34,13 +34,13 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/counter">Menu</NavLink>
+                </NavItem>
+                <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/employees">Employees</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Salaries</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
