2cd04a93b1a2390bf58c7adacd889a1fd8866f8f
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-09-27 14:32:21 -0500

    creating the component Employees

--- WebApplication/Controllers/WeatherForecastController.cs
+++ WebApplication/Controllers/WeatherForecastController.cs
@@ -26,6 +26,8 @@ namespace WebApplication.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            Console.WriteLine("Request received");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
